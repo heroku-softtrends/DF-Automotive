21fe62babf8447661776fe1d8963f7a039ada627
baliles <david@baliles.com>
2016-09-30 07:43:59 -0400

    Final Commit before Heroku Button

--- ConfigVars.cs
+++ ConfigVars.cs
@@ -24,8 +24,8 @@ namespace DemoDFAutomotive
 
             //for local testing
             //VIN = "4A3AK34T58E008515";
-            //IOTToken = "Bearer iguZgpnIwr8N60cD7cYgSMbYQm9QZXEY9AaqmeD6f4d2DoyvZNEhcQdzGSSFivDylWcXR5ShTu1AfMSCJi9sAj";
-            //EnpointUrl = "https://dashboard.us3.sfdcnow.com/clusters/xcdvudaz0dz3/inputstreams/automotive_market002/connections/automotive_event_001/event";
+            //IOTToken = "Bearer 279OC5MYsEqkVwBdWqTMMixlePISTXgfvLUFJJ8luCScsIq8P4svqPfjhEhawnuzAt20KiqvT9ae36XM3YXcFU";
+            //EnpointUrl = "https://ingestion-xcdvudaz0dz3.us3.sfdcnow.com/streams/automotive_sales_001/car_events001/event";
             //IronMQUrl = "https://mq-aws-eu-west-1-1.iron.io/3/projects/57e657540e8c650007ec1ef9/queues/automotive_messages";
             //IronMQToken = "On5WZgi25eKM7Pb7yeL7";
         }
